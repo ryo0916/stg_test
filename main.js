6d53e53d1eba68e9923d30ae4da6f61303f23844
Ryo Kawakami <rkganma@gmail.com>
2019-02-09 23:34:55 +0900

    ショット発射まで

--- main.js
+++ main.js
@@ -4,7 +4,12 @@ var run = true;
 var fps = 1000 / 30;
 var mouse = new Point();
 var ctx; // canvas2d コンテキスト格納用
+var fire = false;
+
+// const 定数
 var CHARA_COLOR = 'rgba(0, 0, 255, 0.75)';
+var CHARA_SHOT_COLOR = 'rgba(0, 255, 0, 0.75)';
+var CHARA_SHOT_MAX_COUNT = 10;
 
 // main
 window.onload = function(){   
@@ -18,6 +23,7 @@ window.onload = function(){
 
     // イベントの登録
     screenCanvas.addEventListener('mousemove', mouseMove, true);
+    screenCanvas.addEventListener('mousedown', mouseDown, true);
     window.addEventListener('keydown', keyDown, true);
 
     // その他のエレメント関連
@@ -26,6 +32,12 @@ window.onload = function(){
     // 自機初期化
     var chara = new Character();
     chara.init(10);
+
+    // ショット初期化
+    var charaShot = new Array(CHARA_SHOT_MAX_COUNT);
+    for(i = 0; i < CHARA_SHOT_MAX_COUNT; i++){
+        charaShot[i] = new CharacterShot();
+    }
 
     // ループ処理を呼び出す
     (function(){
@@ -50,6 +62,51 @@ window.onload = function(){
 
         // 自機を描く
         ctx.fill();
+
+        // fireフラグの値により分岐
+        if(fire){
+            // 全ての自機ショットを調査する
+            for(i = 0; i < CHARA_SHOT_MAX_COUNT; i++){
+                if(!charaShot[i].alive){
+                    // 自機ショットを新規にセット
+                    charaShot[i].set(chara.position, 3, 5);
+
+                    // ループを抜ける
+                    break;
+                }
+            }
+            // フラグを降ろしておく
+            fire = false;
+        }
+
+        // ショットパスの設定を開始
+        ctx.beginPath();
+
+        // 全ての自機ショットを調査する
+        for(i = 0; i < CHARA_SHOT_MAX_COUNT; i++){
+            // 自機ショットが既に発射されているかチェック
+            if(charaShot[i].alive){
+                // 自機ショットを動かす
+                charaShot[i].move();
+
+                // 自機ショットを描くパスを設定
+                ctx.arc(
+                    charaShot[i].position.x,
+                    charaShot[i].position.y,
+                    charaShot[i].size,
+                    0, Math.PI * 2, false
+                );
+
+                // パスをいったん閉じる
+                ctx.closePath();
+            }
+        }
+
+        // 自機ショットの色を設定する
+        ctx.fillStyle = CHARA_SHOT_COLOR;
+
+        // 自機ショットを描く
+        ctx.fill();
         
         // フラグにより再帰呼び出し
         if(run){setTimeout(arguments.callee, fps);}
@@ -69,4 +126,9 @@ function keyDown(event){
 
     // Escキーが押されていたらフラグを降ろす
     if(ck == 27){run = false;}
+}
+
+function mouseDown(event){
+    // フラグを立てる
+    fire = true;
 }
